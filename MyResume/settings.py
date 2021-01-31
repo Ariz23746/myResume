8e5c155bd877e5cc0783aaf42868a4470e20e00c
Mohammad Ariz khan <khan.ariz2341997@gmail.com>
2021-01-31 19:35:10 +0530

    ss

--- MyResume/settings.py
+++ MyResume/settings.py
@@ -125,7 +125,7 @@ EMAIL_PORT = 587
 
 EMAIL_HOST_USER = os.environ.get("MY_EMAIL")
 print(EMAIL_HOST_USER)
-EMAIL_HOST_PASSWORD = os.environ.get("MY_PASS")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
