5369137109de30ead761761cc2b091c668deb9ae
Mohammad Ariz khan <khan.ariz2341997@gmail.com>
2021-01-31 18:07:53 +0530

    updated settings

--- MyResume/settings.py
+++ MyResume/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vwv44l(l)y774rn!+^$_eg2wi*8hey#^w12%l#z%^*#w+#yv+)'
+SECRET_KEY = os.environ.get('ARIZ_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://arizkhan-resume.herokuapp.com/','localhost']
 
 
 # Application definition
